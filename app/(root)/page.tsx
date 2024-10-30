651294002625257c6135e035d3765bfff296b34a
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-10-30 12:03:24 +0100

    folder setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='bg-purple-700 flex w-full flex-row max-xl:max-h-screen max-xl:overflow-y-scrol p-5'>
+
+      <Navbar />
+
+    </section>
+  )
+}
+
+export default Home
---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <section className='flex flex-row p-5 gap-16'>
+            <div>
+                <h1>Dumz.Fun</h1>
+            </div>
+
+            <div className='flex flex-row'>
+                <h1>token blah</h1>
+                <h1>token blah</h1>
+                <h1>token blah</h1>
+                <h1>token blah</h1>
+            </div>
+
+            <div>
+                <button>Connect Wallet</button>
+            </div>
+
+
+        </section>
+    )
+}
+
+export default Navbar
