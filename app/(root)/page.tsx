2a727532e69b8beff05d4e319705a26953bfbba9
Victor Okpukpan <vokpukpan@gmail.com>
2024-11-05 18:35:55 +0100

    feat: add connect to arconnect function & add sendMessageToAO function in create-token page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,3 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
-
 const Home = () => {
   return (
     <section >
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,74 @@
-"use client"
-import React from 'react'
-import { navLinks } from '@/constants'
-import { usePathname } from 'next/navigation'
-import { cn } from '@/lib/utils';
+"use client";
+import React from "react";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
+  const pathname = usePathname();
 
-    const pathname = usePathname();
+  async function connectWallet() {
+    try {
+      // connect to the ArConnect browser extension
+      await window.arweaveWallet.connect(
+        // request permissions
+        ["ACCESS_ADDRESS", "SIGN_TRANSACTION"]
+      );
+    } catch (error) {
+      alert("You should connect to ArConnect browser extension.");
+      return false;
+    }
 
-    return (
+    return true;
+  }
 
-        <section className="flex flex-row justify-between gap-6 w-full bg-black bg-opacity-40 p-5 ">
+  async function getWalletAddress() {
+    let address;
+    try {
+      address = await window.arweaveWallet.getActiveAddress();
+    } catch (error) {
+      return "";
+    }
 
-            <div className=''>
-                <h1 className='font-bold text-24 text-white'>Dumz.Fun</h1>
-            </div>
+    return address;
+  }
 
+  return (
+    <section className="flex flex-row justify-between gap-6 w-full bg-black bg-opacity-40 p-5 ">
+      <div className="">
+        <h1 className="font-bold text-24 text-white">Dumz.Fun</h1>
+      </div>
 
-            <div className="flex flex-row gap-10 ">
-                {navLinks.map((item) => {
-                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+      <div className="flex flex-row gap-10 ">
+        {navLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
 
-                    return (<a href={item.route} key={item.id}>
-                        <h1 className={cn('navlink', { 'bg-bank-gradient': isActive })}
-                        >
-                            {item.label}
-                        </h1>
-                    </a>)
-                })}
-            </div>
+          return (
+            <a href={item.route} key={item.id}>
+              <h1 className={cn("navlink", { "bg-bank-gradient": isActive })}>
+                {item.label}
+              </h1>
+            </a>
+          );
+        })}
+      </div>
 
-            <div className="sm:flex hidden flex-row justify-end gap-4">
-                <button className="text-white bg-blue-600 py-1 px-5 rounded-lg font-bold hover:bg-opacity-80">
-                    connect wallet
-                </button>
-            </div>
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <button
+          onClick={connectWallet}
+          className="text-white bg-blue-600 py-1 px-5 rounded-lg font-bold hover:bg-opacity-80"
+        >
+          connect wallet
+        </button>
+        {/* <ConnectButton
+          accent="rgb(255, 0, 0)"
+          profileModal={true}
+          showBalance={false}
+        /> */}
+      </div>
+    </section>
+  );
+};
 
-
-        </section>
-    )
-}
-
-export default Navbar
+export default Navbar;
